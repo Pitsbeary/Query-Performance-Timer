702ac402eefea40cb0f383e1296959da79f8b172
Pitsbeary <duomisiewitz@gmail.com>
2019-02-24 01:37:45 +0100

    Timer.
    - initial commit.

---
+++ src/timer.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <windows.h>
+
+class QueryPerformanceTimer
+{
+	LARGE_INTEGER counterStart, counterStop;
+	double counterFrequency;
+
+	public:
+		QueryPerformanceTimer();
+
+		void resetTimer();
+
+		inline void startTimer();
+		inline void stopTimer();
+
+		double getElapsedTime();
+		double getCounterFrequency();
+
+	private:
+		void initFrequency();
+
+};
+
+QueryPerformanceTimer::QueryPerformanceTimer()
+{
+	initFrequency();
+
+	resetTimer();
+}
+
+void QueryPerformanceTimer::initFrequency()
+{
+	LARGE_INTEGER li;
+	QueryPerformanceFrequency(&li);
+
+	counterFrequency = double(li.QuadPart);
+}
+
+void QueryPerformanceTimer::resetTimer()
+{
+	LARGE_INTEGER li;
+	QueryPerformanceCounter(&li);
+
+	counterStop = counterStart = li;
+}
+
+inline void QueryPerformanceTimer::startTimer()
+{
+	QueryPerformanceCounter(&counterStart);
+}
+
+inline void QueryPerformanceTimer::stopTimer()
+{
+	QueryPerformanceCounter(&counterStop);
+}
+
+double QueryPerformanceTimer::getElapsedTime()
+{
+	if(counterStart.QuadPart > counterStop.QuadPart)
+	{
+		return 0.0;
+	}
+
+	return static_cast<double>(counterStop.QuadPart - counterStart.QuadPart) / counterFrequency;
+}
+
+double QueryPerformanceTimer::getCounterFrequency()
+{
+	return counterFrequency;
+}
+
+
---
+++ test/test-timer.cpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "catch.hpp"
+#include "../src/timer.h"
+
+TEST_CASE("Timer basic tests.", "[timer][basics]")
+{
+	QueryPerformanceTimer testTimer;
+
+	SECTION( "Created timer should return counter frequency > 0.0" )
+	{
+		REQUIRE(testTimer.getCounterFrequency() > 0.0);
+	}
+
+	SECTION( "Created timer should return 0.0 elapsed time" )
+	{
+		REQUIRE(testTimer.getElapsedTime() == 0.0);
+	}
+
+	SECTION( "getElapsedTime() should return 0.0 when startTimer() was called and stopTimer() was not called after." )
+	{
+		REQUIRE(testTimer.getElapsedTime() == 0.0);
+	}
+
+	SECTION( "getElapsedTime() should return value greater than 0.0 when startTimer() and stopTimer() were called correctly." )
+	{
+		testTimer.startTimer();
+		Sleep(1000);
+		testTimer.stopTimer();
+
+		REQUIRE(testTimer.getElapsedTime() > 0.0);
+		SECTION( "getElapsedTime() should return approximately 1.0 after sleeping for 1000 milliseconds." )
+		{
+			REQUIRE(testTimer.getElapsedTime() == Approx(1.0).margin(0.001));
+		}
+		SECTION( "getElapsedTime() should return 0.0 after calling resetTimer()." )
+		{
+			testTimer.resetTimer();
+
+			REQUIRE(testTimer.getElapsedTime() == 0.0);
+		}
+	}
+}
